4cd16b6e4520fcb612eb8a25aa1a0a5450df79b3
Steven La <stevenla@g.ucla.edu>
2020-02-03 22:24:31 -0800

    created simple GalleryList component

---
+++ frontend/src/pages/GalleryList.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class GalleryList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Gallery List</h1>
+      </div>
+    );
+  }
+}
