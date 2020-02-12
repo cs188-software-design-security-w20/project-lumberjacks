8694d13b7c6cb45c8e92b658bf6e706f4ebe1f81
Steven La <stevenla@g.ucla.edu>
2020-02-12 10:35:45 -0800

    created navbar

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,12 +1,29 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import Home from './pages/Home';
+import AddShortcutContainer from './components/AddShortcutContainer';
+import ViewShortcutContainer from './components/ViewShortcutContainer';
+import { Navbar, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
 
 const App = () => {
   return (
     <Router>
+      <Navbar>
+        <NavbarBrand href="/">home</NavbarBrand>
+        <Nav>
+          <NavItem>
+            <NavLink href="/addShortcut">add shortcut</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/viewShortcut">view shortcut</NavLink>
+          </NavItem>
+        </Nav>
+      </Navbar>
+
       <Switch>
         <Route exact path="/" component={Home} />
+        <Route path="/addShortcut" component={AddShortcutContainer} />
+        <Route path="/viewShortcut" component={ViewShortcutContainer} />
       </Switch>
     </Router>
   );
--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import GalleryList from '../components/GalleryList';
 import Login from './Login';
-import AddShortcutContainer from '../components/AddShortcutContainer';
 
 const Home = () => (
   <div>
     <Login />
     <GalleryList />
-    <AddShortcutContainer />
   </div>
 );
 
