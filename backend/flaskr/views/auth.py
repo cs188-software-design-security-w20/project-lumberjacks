17a5a2918cc056592480f4df14f7c5cf81e4271b
Alexander Xu <xualexander1@gmail.com>
2020-02-12 11:07:41 -0800

    login and logout routes

--- backend/flaskr/views/auth.py
+++ backend/flaskr/views/auth.py
@@ -8,3 +8,21 @@ from . import main
 @main.route('/', methods=['GET', 'POST']) 
 def index():
     return 'Test'
+
+@main.route('/login', methods=['POST'])
+def login();
+    user_info = db_manager.log_in(request.get_json(force=True))
+    if user_info == -1:
+        return {'error': 'User doesn\'t exist'}
+    elif user_info == -2:
+        return {'error': 'Incorrect password'}
+    elif user_info == -3:
+        return {'error': 'Server error'}
+    else:
+        return user_info
+
+@main.route('/logout')
+@login_required
+def logout():
+    db_manager.logout()
+    return 'success'
