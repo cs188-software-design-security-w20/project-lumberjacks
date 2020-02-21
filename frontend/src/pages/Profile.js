2cb8ca49d4fb570f99e2b273160b0de77cade8db
Alexander Xu <xualexander1@gmail.com>
2020-02-21 01:36:09 -0800

    fix gallery style

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -2,36 +2,37 @@ import React, { useEffect, useState } from 'react';
 import Core from '../api_clients/core';
 
 import ShortcutCard from '../components/ShortcutCard';
+import { StyledContainer, StyledHeaderText } from '../components/styles';
 
 const Profile = ({}) => {
-  const [links, setLinks] = React.useState(null);
-  useEffect(() => {
-    (async function fetchGallery() {
-      const gallery = await Core.getGallery();
-      setLinks(gallery);
-    })();
-  }, [Core.getGallery, setLinks]);
-  return (
-    <div>
-      {links == null
-        ? null
-        : links.map(linkObject => (
-            <ShortcutCard
-              key={linkObject.id}
-              style={{
-                boxShadow: 'none',
-                borderBottom: 'gray .5px solid',
-                borderBottomLeftRadius: 0,
-                borderBottomRightRadius: 0,
-              }}
-              urls={linkObject.links.split(',')}
-              id={linkObject.id}
-              shortcutName={linkObject.name}
-              macro={'/' + linkObject.shortlink}
-            />
-          ))}
-    </div>
-  );
+	const [ links, setLinks ] = React.useState(null);
+	useEffect(
+		() => {
+			(async function fetchGallery() {
+				const gallery = await Core.getGallery();
+				setLinks(gallery);
+			})();
+		},
+		[ Core.getGallery, setLinks ]
+	);
+	return (
+		<StyledContainer>
+			<StyledHeaderText>My macros</StyledHeaderText>
+			<div>
+				{links == null ? null : (
+					links.map((linkObject) => (
+						<ShortcutCard
+							key={linkObject.id}
+							urls={linkObject.links.split(',')}
+							id={linkObject.id}
+							shortcutName={linkObject.name}
+							macro={'/' + linkObject.shortlink}
+						/>
+					))
+				)}
+			</div>
+		</StyledContainer>
+	);
 };
 
 export default Profile;
