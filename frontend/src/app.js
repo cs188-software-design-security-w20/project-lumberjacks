bd1317e57bf0b9de14106b11a5877ad928b53149
Bryan Wong <bryanw0ng@ucla.edu>
2020-02-20 23:11:10 -0800

    add navbar

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,5 +1,11 @@
 import React, { useEffect, useState } from 'react';
-import { Redirect, Route, BrowserRouter as Router, Switch, useHistory } from 'react-router-dom';
+import {
+  Redirect,
+  Route,
+  BrowserRouter as Router,
+  Switch,
+  useHistory,
+} from 'react-router-dom';
 import Home from './pages/Home';
 import AddShortcutContainer from './components/AddShortcutContainer';
 import ProfileContainer from './components/ProfileContainer';
@@ -10,72 +16,128 @@ import { Navbar, NavbarBrand, Nav, NavItem, NavLink, Button } from 'reactstrap';
 import Auth from './api_clients/auth';
 
 const App = () => {
-	const history = useHistory();
-	const [ isLoggedIn, setLoggedIn ] = useState(true);
-	const [ user, setUser ] = useState(null);
+  const history = useHistory();
+  const [isLoggedIn, setLoggedIn] = useState(true);
+  const [user, setUser] = useState(null);
 
-	async function authUser() {
-		const data = await Auth.auth();
-		console.log(data);
-		if (!data['error']) {
-			setLoggedIn(true);
-			setUser(data.name);
-		} else {
-			setLoggedIn(false);
-		}
-	}
+  async function authUser() {
+    const data = await Auth.auth();
+    console.log(data);
+    if (!data['error']) {
+      setLoggedIn(true);
+      setUser(data.name);
+    } else {
+      setLoggedIn(false);
+    }
+  }
 
-	async function logout() {
-		await Auth.logout();
-		await authUser();
-		history.push('/');
-	}
+  async function logout() {
+    await Auth.logout();
+    await authUser();
+    history.push('/');
+  }
 
-	useEffect(async () => await authUser(), []);
+  useEffect(async () => await authUser(), []);
 
-	return (
-		<Router>
-			<Navbar>
-				<NavbarBrand href="/">home</NavbarBrand>
-				<Nav>
-					<NavItem>
-						<NavLink href="/addShortcut">Add shortcut</NavLink>
-					</NavItem>
-					<NavItem>
-						<NavLink href="/profile">Profile</NavLink>
-					</NavItem>
-					<NavItem>
-						<NavLink href="/feed">Feed</NavLink>
-					</NavItem>
-					{!isLoggedIn && (
-						<NavItem>
-							<NavLink href="/login">Sign in</NavLink>
-						</NavItem>
-					)}
-					{isLoggedIn && (
-						<NavItem>
-							<NavLink onClick={() => logout()}>Sign out</NavLink>
-						</NavItem>
-					)}
-				</Nav>
-			</Navbar>
+  const navItemStyle = {
+    color: 'rgba(0,0,0, .9)',
+    textDecoration: 'none',
+    fontFamily: 'sans-serif',
+    fontSize: '1rem',
+    cursor: 'pointer',
+  };
 
-			<Switch>
-				<Route exact path="/">
-					{isLoggedIn ? <Redirect to="/feed" /> : <Home authUser={authUser} history={history} />}
-				</Route>
-				<Route path="/login">
-					<Home authUser={authUser} history={history} />
-				</Route>
-				<Route path="/addShortcut">{isLoggedIn ? <AddShortcutContainer /> : <Redirect to="/" />}</Route>
-				<Route path="/profile"> {isLoggedIn ? <ProfileContainer /> : <Redirect to="/" />}</Route>
-				<Route path="/feed">
-					<FeedContainer />
-				</Route>
-				<Route path="/:shortlink" children={<ShortlinkRedirectContainer />} />
-			</Switch>
-		</Router>
-	);
+  document.body.style.backgroundColor = '#f9f9f9';
+  document.body.style.margin = 0;
+
+  return (
+    <Router>
+      <Navbar
+        style={{
+          padding: 20,
+          width: '100%',
+          borderBottom: '.5px darkgray solid',
+          marginBottom: 50,
+        }}
+      >
+        <Nav
+          style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            maxWidth: '30%',
+            color: 'black',
+            listStyleType: 'none',
+            padding: 0,
+            marginTop: 5,
+            marginBottom: 5,
+          }}
+        >
+          <NavItem>
+            <NavLink style={navItemStyle} href="/">
+              Home
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink style={navItemStyle} href="/addShortcut">
+              Add shortcut
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink style={navItemStyle} href="/profile">
+              Profile
+            </NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink style={navItemStyle} href="/feed">
+              Feed
+            </NavLink>
+          </NavItem>
+          {!isLoggedIn && (
+            <NavItem>
+              <NavLink style={navItemStyle} href="/login">
+                Sign in
+              </NavLink>
+            </NavItem>
+          )}
+          {isLoggedIn && (
+            <NavItem>
+              <NavLink
+                style={navItemStyle}
+                onClick={() => logout()}
+                href="/login"
+              >
+                Sign out
+              </NavLink>
+            </NavItem>
+          )}
+        </Nav>
+      </Navbar>
+
+      <Switch>
+        <Route exact path="/">
+          {isLoggedIn ? (
+            <Redirect to="/feed" />
+          ) : (
+            <Home authUser={authUser} history={history} />
+          )}
+        </Route>
+        <Route path="/login">
+          <Home authUser={authUser} history={history} />
+        </Route>
+        <Route path="/addShortcut">
+          {isLoggedIn ? <AddShortcutContainer /> : <Redirect to="/" />}
+        </Route>
+        <Route path="/profile">
+          {' '}
+          {isLoggedIn ? <ProfileContainer /> : <Redirect to="/" />}
+        </Route>
+        <Route path="/feed">
+          <FeedContainer />
+        </Route>
+        <Route path="/:shortlink" children={<ShortlinkRedirectContainer />} />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
