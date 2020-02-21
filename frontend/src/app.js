f60f246aa7120e0fef2f9f54bb7aab8d4e14d315
Alexander Xu <xualexander1@gmail.com>
2020-02-20 22:45:00 -0800

    fix again

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,14 +1,5 @@
 import React, { useEffect, useState } from 'react';
-<<<<<<< HEAD
 import { Redirect, Route, BrowserRouter as Router, Switch, useHistory } from 'react-router-dom';
-=======
-import {
-  Redirect,
-  Route,
-  BrowserRouter as Router,
-  Switch,
-} from 'react-router-dom';
->>>>>>> f5aa103ad7ed30725b745f1c3f6e03dbff0395ea
 import Home from './pages/Home';
 import AddShortcutContainer from './components/AddShortcutContainer';
 import ProfileContainer from './components/ProfileContainer';
@@ -19,46 +10,29 @@ import { Navbar, NavbarBrand, Nav, NavItem, NavLink, Button } from 'reactstrap';
 import Auth from './api_clients/auth';
 
 const App = () => {
-<<<<<<< HEAD
 	const history = useHistory();
 	const [ isLoggedIn, setLoggedIn ] = useState(true);
 	const [ user, setUser ] = useState(null);
-=======
-  const [isLoggedIn, setLoggedIn] = useState(true);
-  const [tryLogin, setTryLogin] = useState(true);
-  const [user, setUser] = useState(null);
->>>>>>> f5aa103ad7ed30725b745f1c3f6e03dbff0395ea
 
-  async function authUser() {
-    const data = await Auth.auth();
-    console.log(data);
-    if (!data['error']) {
-      setLoggedIn(true);
-      setUser(data.name);
-    } else {
-      setLoggedIn(false);
-    }
-  }
+	async function authUser() {
+		const data = await Auth.auth();
+		console.log(data);
+		if (!data['error']) {
+			setLoggedIn(true);
+			setUser(data.name);
+		} else {
+			setLoggedIn(false);
+		}
+	}
 
-<<<<<<< HEAD
 	async function logout() {
 		await Auth.logout();
 		await authUser();
 		history.push('/');
 	}
-=======
-  async function logout() {
-    if (!tryLogin) {
-      return;
-    }
-    await Auth.logout();
-    authUser();
-  }
->>>>>>> f5aa103ad7ed30725b745f1c3f6e03dbff0395ea
 
-  useEffect(async () => await authUser(), []);
+	useEffect(async () => await authUser(), []);
 
-<<<<<<< HEAD
 	return (
 		<Router>
 			<Navbar>
@@ -102,51 +76,6 @@ const App = () => {
 			</Switch>
 		</Router>
 	);
-=======
-  // lol css
-  document.body.style.backgroundColor = '#f9f9f9';
-
-  return (
-    <Router>
-      <Navbar>
-        <NavbarBrand href="/">home</NavbarBrand>
-        <Nav>
-          <NavItem>
-            <NavLink href="/addShortcut">Add shortcut</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="/profile">Profile</NavLink>
-          </NavItem>
-          <NavItem>
-            <NavLink href="/feed">Feed</NavLink>
-          </NavItem>
-          {isLoggedIn && (
-            <NavItem>
-              <NavLink onClick={() => logout()}>Sign out</NavLink>
-            </NavItem>
-          )}
-        </Nav>
-      </Navbar>
-
-      <Switch>
-        <Route exact path="/">
-          {isLoggedIn ? <Redirect to="/feed" /> : <Home authUser={authUser} />}
-        </Route>
-        <Route path="/addShortcut">
-          {isLoggedIn ? <AddShortcutContainer /> : <Redirect to="/" />}
-        </Route>
-        <Route path="/profile">
-          {' '}
-          {isLoggedIn ? <ProfileContainer /> : <Redirect to="/" />}
-        </Route>
-        <Route path="/feed">
-          <FeedContainer />
-        </Route>
-        <Route path="/:shortlink" children={<ShortlinkRedirectContainer />} />
-      </Switch>
-    </Router>
-  );
->>>>>>> f5aa103ad7ed30725b745f1c3f6e03dbff0395ea
 };
 
 export default App;
