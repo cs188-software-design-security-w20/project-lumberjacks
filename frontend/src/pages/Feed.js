af383dd5c5af242b2dab9d1afd15a13ecfe21034
Michael Yu <michael.yu2016@gmail.com>
2020-02-21 01:14:49 -0800

    Rebase

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -1,34 +1,79 @@
 import React, { useEffect, useState } from 'react';
 import ShortcutCard from '../components/ShortcutCard';
+import AddShortcutContainer from './AddShortcut';
+
+import { StyledContainer, StyledHeaderText } from '../components/styles';
+
 import Core from '../api_clients/core';
+import Modal from 'react-modal';
 
 const FeedContainer = ({}) => {
-  const [links, setLinks] = useState(null);
-  useEffect(() => {
-    (async function fetchGallery() {
-      const gallery = await Core.getGallery();
-      setLinks(gallery);
-    })();
-  }, []);
+  // Fetch feed, pass to component
+  const [links, setLinks] = useState([]);
+  const [offset, setOffset] = useState(0);
+  const [forkModal, openFork] = useState(false);
+  const [repostName, setRepostName] = useState('');
+  const [repostLinks, setRepostLinks] = useState([]);
+  const [repostId, setRepostId] = useState(-1);
+
+  async function getLinks() {
+    try {
+      const data = await Core.getFeed(offset, 10, 0);
+      setLinks(data);
+    } catch (err) {
+      return;
+    }
+  }
+
+  async function getLinkInfo(id) {
+    try {
+      const data = await Core.getLink(id);
+      console.log(data);
+      setRepostId(id);
+      setRepostLinks(data['links'].split(','));
+      setRepostName(data['name']);
+    } catch (err) {
+      return;
+    }
+  }
+
+  async function fork(id) {
+    try {
+      await getLinkInfo(id);
+      console.log(repostName, repostLinks, repostId);
+      openFork(true);
+    } catch (err) {
+      return;
+    }
+  }
+
+  useEffect(() => getLinks(), []);
+
   return (
     <div>
-      {links == null
-        ? null
-        : links.map(linkObject => (
+      <Modal isOpen={forkModal} onRequestClose={() => openFork(false)}>
+        <AddShortcutContainer
+          forking
+          repostId
+          filledLinks={repostLinks}
+          repostName
+        />
+      </Modal>
+      <StyledContainer>
+        <StyledHeaderText>Feed</StyledHeaderText>
+        <div>
+          {links.map(link => (
             <ShortcutCard
-              key={linkObject.id}
-              style={{
-                boxShadow: 'none',
-                borderBottom: 'gray .5px solid',
-                borderBottomLeftRadius: 0,
-                borderBottomRightRadius: 0,
-              }}
-              urls={linkObject.links.split(',')}
-              id={linkObject.id}
-              shortcutName={linkObject.name}
-              macro={'/' + linkObject.shortlink}
+              id={link.id}
+              macro={link.shortlink}
+              urls={link.links.split(',')}
+              shortcutName={link.name}
+              forkable
+              fork={() => fork(link.id)}
             />
           ))}
+        </div>
+      </StyledContainer>
     </div>
   );
 };
